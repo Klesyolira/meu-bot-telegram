49bf60888b9d214c80fbab7aaecacc7f5eb355e5
Lughlt <168665654+Klesyolira@users.noreply.github.com>
2026-06-06 11:38:07 -0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,16 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+# Pega o token das variáveis de ambiente do Render
+TOKEN = os.environ.get("8623547073:AAGlpw7UpJfb7FTG4nUxAc_8judTjz058PM")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Painel Yoru Shield conectado com sucesso!')
+
+if __name__ == '__main__':
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Bot rodando...")
+    app.run_polling()
